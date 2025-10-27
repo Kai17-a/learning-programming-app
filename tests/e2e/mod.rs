87de8a492c34f3d2ab667c2ec6f53a6693e62f0b
Kai17-a <k.s25skt@gmail.com>
2025-10-27 22:33:01 +0900

    init

--- tests/e2e/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod error_recovery_test;
-pub mod full_workflow_test;
-pub mod performance_test;
-pub mod system_integration_test;
--- tests/e2e_tests.rs
+++
@@ -1,4 +0,0 @@
-mod e2e;
-
-// Re-export all e2e tests
-pub use e2e::*;
